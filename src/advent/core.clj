53fd520d464335464562023ca4096c29b93d5022
John Gorski <john.gorski.jr@gmail.com>
2019-10-22 22:37:11 -0700

    2017/1

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,6 +1,45 @@
-(ns advent.core)
+(ns advent.core
+  (:require [clojure.edn :as edn]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn diff-circ-sum
+  [[curr & rest :as list] last acc]
+  (if (empty? list)
+    acc
+    (recur rest curr (if (= last curr)
+                       (+ curr acc)
+                       acc))))
+
+(edn/read-string "4")
+(clojure.string/split "1234" #"")
+
+(defn wrap
+  "Puts the first character back on the end."
+  [s]
+  (str s (first s)))
+
+(defn nums
+  "Digits in the string as ints."
+  [s]
+  (map edn/read-string (clojure.string/split s #"")))
+
+(defn rev-cap
+  "the sum of all digits that match the next digit in the list. The list is circular, so the digit after the last digit is the first digit in the list."
+  [s]
+  (->
+   s
+   wrap
+   nums
+   (diff-circ-sum nil 0)))
+
+(defn shift
+  "xs shifted over by one half of the input length"
+  [xs]
+  (let [half (/ (count xs) 2)]
+    (concat (drop half xs) (take half xs))))
+
+(defn circ-cap
+  "Part 2 checksum."
+  [s]
+  (let [xs (nums s)
+        shifted (shift xs)]
+    (reduce + 0 (map #(if (= %1 %2) %1 0) xs shifted))))
