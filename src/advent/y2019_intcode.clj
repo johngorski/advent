792f093031ba94ab5c30019f11d7fa1ee5808559
John Gorski <john.gorski.jr@gmail.com>
2019-12-05 23:54:26 -0800

    Good start on 2019 intcode tests and some cleanup. A few more tests and it'll be time to refactor the intcode decode function

--- src/advent/y2019_intcode.clj
+++ src/advent/y2019_intcode.clj
@@ -4,67 +4,6 @@
    [clojure.java.io :as io]
    [clojure.string :as string]))
 
-
-(def in-2 (map edn/read-string (string/split (slurp (io/resource "2019/2.txt")) #",")))
-
-(defn parse
-  "Returns a function transforming the state of memory to the next state per the current instruction."
-  [[op fst snd dest]]
-  (when-let [operator ({1 +, 2 *, 99 nil} op)]
-    (fn [memory] (assoc memory dest (operator (get memory fst) (get memory snd))))))
-
-(defn run-mem
-  "Returns the state after running initial state mem from position pc."
-  [pc mem]
-  (if-let [op (parse (subvec mem pc (+ 4 pc)))]
-    (recur (+ 4 pc) (op mem))
-    {:memory mem
-     :halted-with (get mem pc)}))
-
-(def sample-2 [1,9,10,3,2,3,11,0,99,30,40,50])
-
-(comment
-  (run-mem 0 sample-2)
-  ((parse [1 0 0 0]) [1 0 0 0 99])
-  (run-mem 0 [1,0,0,0,99, 0 ,0 0])
-  (run-mem 0 [2,3,0,3,99 0 0 0])
-  (run-mem 0 [2,4,4,5,99,0 0 0])
-  (run-mem 0 [1,1,1,4,99,5,6,0,99 0 0 0])
-
-  (run-mem 0 (-> (vec in-2)
-                 (assoc 1 12)
-                 (assoc 2 2)
-                 ))
-  )
-(comment not 644274)
-(comment 8017076)
-
-(defn output [noun verb]
-  (get-in
-   (run-mem 0 (-> (vec in-2)
-                  (assoc 1 noun)
-                  (assoc 2 verb)
-                  ))
-   [:memory 0]))
-
-(comment
-  (output 12 2)
-  )
-(comment
-  (->> (for [noun (range 100)
-             verb (range 100)]
-         [noun verb])
-       (filter #(= 19690720 (output (first %) (second %))))
-       )
-
-  [31 46]
-  )
-
-(comment
-  (reduce-kv #(assoc %1 %2 %3) [0 0 0] [1 0 1])
-  (vec (repeat 3 identity))
-  )
-
 (defn decode
   "Decodes the instruction from the memory pointer, yielding update functions
   for the program counter (pc) and for the memory itself"
@@ -145,52 +84,5 @@
     (recur (step computer))
     computer))
 
+(def in-2 (map edn/read-string (string/split (slurp (io/resource "2019/2.txt")) #",")))
 (def in-5 (vec (map edn/read-string (string/split (slurp (io/resource "2019/5.txt")) #","))))
-
-;; Day 5
-(comment
-  (run-intcode {:pc 0 :mem [3,0,4,0,99] :input [69]}) ;; nice
-  (run-intcode {:pc 0 :mem [1002,4,3,4,33]})
-  (:output (run-intcode {:pc 0, :input [1], :mem in-5}))
-  )
-;; 13087969
-
-(:output (run-intcode {:pc 0, :mem [3,9,8,9,10,9,4,9,99,-1,8], :input [8]}))
-(run-intcode {:pc 0, :mem [3,9,8,9,10,9,4,9,99,-1,8], :input [8]})
-(:output (run-intcode {:pc 0, :mem [3,9,8,9,10,9,4,9,99,-1,8], :input [7]}))
-(run-intcode {:pc 0, :mem [3,9,8,9,10,9,4,9,99,-1,8], :input [7]})
-
-
-(comment
-  (:output (run-intcode {:pc 0, :input [5], :mem in-5}))
-  ;; 14110739
-  )
-;; end day 5
-
-(comment
-  (run-intcode {:pc 0 :mem (vec in-2)})
-  (run-intcode {:pc 0
-                :mem (-> (vec in-2)
-                         (assoc 1 31)
-                         (assoc 2 46))})
-  (step {:pc 0
-         :mem (-> (vec in-2)
-                  (assoc 1 31)
-                  (assoc 2 46))})
-  )
-(comment
-  (run-intcode {:pc 0 :mem sample-2})
-  (run-intcode {:pc 0 :mem [1,0,0,0,99]})
-  (run-intcode {:pc 0 :mem [2,3,0,3,99]})
-  (run-intcode {:pc 0 :mem [2,4,4,5,99]})
-  (run-intcode {:pc 0 :mem [1,1,1,4,99,5,6,0,99]})
-
-  (run-intcode {:pc 0 :mem (-> (vec in-2)
-                               (assoc 1 12)
-                               (assoc 2 2)
-                               )})
-
-  (run-intcode {:pc 0 :mem (-> (vec in-2)
-                               (assoc 1 31)
-                               (assoc 2 46))})
-  )
--- test/advent/y2019_intcode_test.clj
+++ test/advent/y2019_intcode_test.clj
@@ -5,6 +5,7 @@
 
 (deftest intcode-computer
   (testing "passes day 5"
+    (is (= 13087969 (last (:output (run-intcode {:pc 0, :input [1], :mem in-5})))))
     (is (= 14110739 (first (:output (run-intcode {:pc 0, :input [5], :mem in-5}))))))
   (testing "day 2 samples pass"
     (is (= 3500 (first (:mem (run-intcode {:pc 0 :mem [1,9,10,3,2,3,11,0,99,30,40,50]})))))
@@ -13,7 +14,26 @@
     (is (= 9801 (last (:mem (run-intcode {:pc 0 :mem [2,4,4,5,99]})))))
     (is (= [30,1,1,4,2,5,6,0,99] (:mem (run-intcode {:pc 0 :mem [1,1,1,4,99,5,6,0,99]})))))
   (testing "day 2 part 2's solution computes"
-    (is (= 19690720 (first (:mem (run-intcode {:pc 0 :mem (-> (vec in-2) (assoc 1 31) (assoc 2 46))})))))))
+    (is (= 19690720 (first (:mem (run-intcode {:pc 0 :mem (-> (vec in-2) (assoc 1 31) (assoc 2 46))}))))))
+  (testing "mode samples"
+    (is (= 99 (last (:mem (run-intcode {:pc 0 :mem [1002,4,3,4,33]})))))
+    (is (= 99 (last (:mem (run-intcode {:pc 0 :mem [1101,100,-1,4,0]}))))))
+  (testing "intcode i/o"
+    (is (= :the-input (first (:output (run-intcode {:pc 0 :mem [3,0,4,0,99] :input [:the-input]}))))))
+  (testing "program using position mode to output 1 iff input = 8"
+    (is (= 1 (first (:output (run-intcode {:pc 0, :mem [3,9,8,9,10,9,4,9,99,-1,8], :input [8]})))))
+    (is (= 0 (first (:output (run-intcode {:pc 0, :mem [3,9,8,9,10,9,4,9,99,-1,8], :input [7]}))))))
+  (testing "position mode for less than"
+    (letfn [(less-than-8 [input] (first (:output (run-intcode {:pc 0, :mem [3,9,7,9,10,9,4,9,99,-1,8], :input [input]}))))]
+      (do
+        (is 1 (less-than-8 7))
+        (is 0 (less-than-8 8))
+        (is 0 (less-than-8 9)))))
+  ;; TODO: immediate mode tests for equal and less than; position+immediate jump tests; larger day 5 part 2 sample.
+  ;; TODO: The test file could use some cleanup, too. I mean maybe? Very strong candidate for tests that should never have to change.
+  ;; Refactoring these would be an intellectual exercise only. Or Capernaum use case. Who knows?
+  )
+
   
 
 
