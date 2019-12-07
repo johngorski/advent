69ff0c90ecd5649c7917bb0a529fdc700140639e
John Gorski <john.gorski.jr@gmail.com>
2019-12-07 15:39:50 -0800

    Use deref where applicable

--- src/advent/y2019_intcode.clj
+++ src/advent/y2019_intcode.clj
@@ -8,9 +8,9 @@
   "Decodes the instruction from the memory pointer, yielding update functions
   for the program counter (pc) and for the memory itself"
   [{:keys [pc mem input]}]
-  (let [op     (get mem pc)
+  (let [deref  #(get mem %)
+        op     (deref pc)
         opcode (rem op 100)
-        deref  #(get mem %)
         modes  (reduce-kv
                 #(assoc %1 %2 %3)
                 (vec (repeat 3 deref))
@@ -22,27 +22,27 @@
     (case opcode
       1 ;; add
       {:pc #(+ 4 %)
-       :mem (let [fst (get mem (+ 1 pc))
-                  snd (get mem (+ 2 pc))
-                  dst (get mem (+ 3 pc))]
+       :mem (let [fst (deref (+ 1 pc))
+                  snd (deref (+ 2 pc))
+                  dst (deref (+ 3 pc))]
               #(assoc % dst (+ ((get modes 0) fst) ((get modes 1) snd))))}
       2 ;; multiply
       {:pc #(+ 4 %)
-       :mem (let [fst (get mem (+ 1 pc))
-                  snd (get mem (+ 2 pc))
-                  dst (get mem (+ 3 pc))]
+       :mem (let [fst (deref (+ 1 pc))
+                  snd (deref (+ 2 pc))
+                  dst (deref (+ 3 pc))]
               #(assoc % dst (* ((get modes 0) fst) ((get modes 1) snd))))}
       3 ;; store from input
       (if (empty? input)
         {:halted (fn [_] :awaiting-input)}
         (let [[first-input & rest-input] input]
           {:pc #(+ 2 %)
-           :mem (let [fst (get mem (inc pc))]
+           :mem (let [fst (deref (inc pc))]
                   #(assoc % fst first-input))
            :input (fn [_] rest-input)}))
       4 ;; output
       {:pc #(+ 2 %)
-       :output (let [fst (get mem (inc pc))]
+       :output (let [fst (deref (inc pc))]
                  #(conj (or % []) ((get modes 0) fst)))}
       5 ;; jump-if-true
       {:pc (let [fst ((get modes 0) (deref (+ 1 pc)))
