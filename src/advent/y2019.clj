5bd191702dbf221997aac6026ecf5f4cef8da0bd
John Gorski <john.gorski.jr@gmail.com>
2019-12-07 12:10:20 -0800

    Part 2 on LOCK. What a bug. Type-checker would have caught it. REPL debugging and such.

--- src/advent/y2019.clj
+++ src/advent/y2019.clj
@@ -570,7 +570,7 @@ I)SAN")
                          computer/run-intcode)
           {:keys [output label halted]} active-amp]
       (if (and (= last-amp-label label) (= :finished halted))
-        output
+        (first output)
         (recur
          output
          (if (= :awaiting-input halted)
@@ -596,5 +596,14 @@ I)SAN")
 
 (comment
 (thrust-from-amp-loop (:mem sample-7-2) (:phases sample-7-2)) ;; => 139629729
-
+(thrust-from-amp-loop [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10
+] [9,7,8,5,6]) ;; => 18216
 )
+
+(apply max (map #(thrust-from-amp-loop in-7 %) (permutations [] (into #{} (range 5 10))))) ; => 17519904
+
+;; TODO: Error state when intcode computer crashes. More programs to test.
+;; Messed up: should have tested halting on input first!
+;; May want to prepare for other intcode-running mappings.
