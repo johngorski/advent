a6ed5b82ad13f704eb0ee1ef156eb615b565a72f
John Gorski <john.gorski.jr@gmail.com>
2019-12-07 16:06:06 -0800

    Decode with increment operator

--- src/advent/y2019_intcode.clj
+++ src/advent/y2019_intcode.clj
@@ -10,6 +10,7 @@
   [{:keys [pc mem input]}]
   (let [deref  #(get mem %)
         =>     (fn [lifted] (fn [_] lifted))
+        ++     (fn [d] (fn [n] (+ n d)))
         op     (deref pc)
         opcode (rem op 100)
         modes  (reduce-kv
@@ -19,16 +20,18 @@
                  (map #(case %
                          \0 deref
                          \1 identity)
-                      (reverse (seq (str (quot op 100)))))))]
+                      (reverse (seq (str (quot op 100)))))))
+        param  (fn [idx] ((get modes idx) (+ 1 pc idx))) ;; get param by mode, 0-indexed after pc
+        ]
     (case opcode
       1 ;; add
-      {:pc #(+ 4 %)
+      {:pc (++ 4)
        :mem (let [fst (deref (+ 1 pc))
                   snd (deref (+ 2 pc))
                   dst (deref (+ 3 pc))]
               #(assoc % dst (+ ((get modes 0) fst) ((get modes 1) snd))))}
       2 ;; multiply
-      {:pc #(+ 4 %)
+      {:pc (++ 4)
        :mem (let [fst (deref (+ 1 pc))
                   snd (deref (+ 2 pc))
                   dst (deref (+ 3 pc))]
@@ -42,7 +45,7 @@
                   #(assoc % fst first-input))
            :input (=> rest-input)}))
       4 ;; output
-      {:pc #(+ 2 %)
+      {:pc (++ 2)
        :output (let [fst (deref (inc pc))]
                  #(conj (or % []) ((get modes 0) fst)))}
       5 ;; jump-if-true
@@ -58,13 +61,13 @@
                 snd
                 (+ 3 %)))}
       7 ;; less than
-      {:pc #(+ 4 %)
+      {:pc (++ 4)
        :mem #(let [fst ((get modes 0) (deref (+ 1 pc)))
                    snd ((get modes 1) (deref (+ 2 pc)))
                    dst (deref (+ 3 pc))]
                (assoc % dst (if (< fst snd) 1 0)))}
       8 ;; equals
-      {:pc #(+ 4 %)
+      {:pc (++ 4)
        :mem #(let [fst ((get modes 0) (deref (+ 1 pc)))
                    snd ((get modes 1) (deref (+ 2 pc)))
                    dst (deref (+ 3 pc))]
