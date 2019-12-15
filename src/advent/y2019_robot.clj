5e78867e0a453c233e446b992c79e485354363de
John Gorski <john.gorski.jr@gmail.com>
2019-12-14 18:45:17 -0800

    Have an almost-working robot: Doesn't process final output. 'Think' state should manage its CPU's halt state.

--- src/advent/y2019_robot.clj
+++ src/advent/y2019_robot.clj
@@ -3,12 +3,147 @@
    [advent.y2019 :as y2019]
    [advent.y2019-intcode :as computer]))
 
-;; Robot starts running its computer
+(def brain (-> (y2019/puzzle-in 11) computer/load-program))
 
+;; State node functions, returning their result condition and the updated world.
+;; Result conditions should be a member of the "condition" column of the state
+;; transition table.
+(defn think [{:keys [brain] :as world}]
+  (let [brain'    (computer/run-intcode brain)
+        world'    (assoc world :brain brain')
+        condition (case (:halted brain')
+                    :finished       :finished
+                    :awaiting-input :need-input)]
+    [condition world']))
+
+(defn append
+  "A conj which treats nil as an empty vector"
+  [coll x]
+  (if (empty? coll)
+    [x]
+    (vec (conj coll x))))
+
+(comment
+(rest []) ;; => ();
+(update (update {} :input conj 0) :input conj 1) ;; => {:input (1 0)};
+(update {} :input append 0) ;; => {:input [0]};
+(update (update {} :input append 0) :input append 1) ;; => {:input [0 1]};
+)
+
+(defn current-panel [{:keys [position panels] :as world}]
+  (get panels position 0))
+
+(comment
+(current-panel {:panels {} :position [0 0]}) ;; => 0;
+(current-panel {:panels {[0 0] 1} :position [0 0]}) ;; => 1;
+)
+
+(defn paint [{:keys [brain panels position] :as world}]
+  (let [color   (first (:output brain))
+        brain'  (update brain :output rest)
+        panels' (assoc panels position color)
+        world'  (assoc world
+                       :brain  brain'
+                       :panels panels')]
+    [:any world']))
+
+(defn move
+  [direction [x y]]
+  (case direction
+    :up    [x (inc y)]
+    :right [(inc x) y]
+    :down  [x (dec y)]
+    :left  [(dec x) y]))
+
+(move :up [0 0]) ;; => [0 1];
+
+(defn turn
+  [command direction]
+  (case command
+    0 (case direction
+        :up    :left
+        :right :up
+        :down  :right
+        :left  :up)
+    
+    1 (case direction
+        :up    :right
+        :right :down
+        :down  :left
+        :left  :up)))
+
+(defn turn-move [{:keys [brain position direction] :as world}]
+  (let [command    (first (:output brain))
+        brain'     (update brain :output rest)
+        direction' (turn command direction)
+        position'  (move direction' position)
+        world'     (assoc world
+                          :brain         brain'
+                          :direction direction'
+                          :position   position')]
+    [:any world']))
+
+(defn process-output [{:keys [brain last-action] :as world}]
+  (if (empty? (:output brain))
+    (let [brain'  (-> brain
+                      (update :input append (current-panel world))
+                      (dissoc :halted))
+          world'  (assoc world :brain brain')
+          condition :output-empty]
+      [condition world'])
+    (let [last-action' (case last-action
+                         :painted-last :moved-last
+                         :moved-last   :painted-last)]
+      [last-action (assoc world :last-action last-action')])))
+
+(def state-transitions
+  ;; start -> condition -> next
+  {:think          {:need-input   :process-output
+                   :finished      :done}
+   :process-output {:output-empty :think
+                   :moved-last    :paint
+                   :painted-last  :turn-move}
+   :paint          {:any          :process-output}
+   :turn-move      {:any          :process-output}})
+
+(def effect
+  {:think          think
+   :process-output process-output
+   :paint          paint
+   :turn-move      turn-move})
+
+(comment
+(= think think) ;; => true;
+(= think (get-in state-transitions [process-output :output-empty])) ;; => true;
+)
+
+(defn state-machine
+  "Transitions from the current state to the next state until it's done, outputting the final state of the world."
+  [state world]
+  (if (= :done state)
+    world
+    (let [[condition world'] ((effect state) world)
+          next               (get-in state-transitions [state condition]
+                                     (fn [_]
+                                       (throw (ex-info "State transition missing" {:state state :condition condition }))))]
+      (recur next world'))))
+
+(get-in state-transitions [:think :need-input]) ;; => :process-output;
+
+(def starting-world
+  {:brain       brain
+   :position    [0 0]
+   :direction   :up
+   :panels      {}
+   :last-action :moved-last})
+
+(state-machine :think starting-world)
+
+(comment ;; pre-paper try
 ;; Robot has a brain, position, and direction.
 ;; In the world, there's a robot and a bunch of panels.
 
-(def brain (-> (y2019/puzzle-in 11) computer/load-program))
+
 
 (defn colors-of
   [panels]
@@ -51,12 +186,6 @@
     :moving
     :painting))
 
-(def starting-world
-  {:robot-brain     brain
-   :robot-position  [0 0]
-   :robot-direction :up
-   :panels          {}
-   :action          :painting})
 
 (defn act-on-world
   "function transforming the world based on the list of commands"
@@ -105,3 +234,4 @@
 
 ;; Halts as finished => we're done
 
+)
