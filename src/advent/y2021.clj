c10ce885f1db61beb935efd1ef0a6aa2b05201eb
John Gorski <john.gorski.jr@gmail.com>
2021-12-02 01:15:36 -0800

    2021 day 2

--- src/advent/y2021.clj
+++ src/advent/y2021.clj
@@ -29,4 +29,77 @@
 
 (comment
   (num-window-increases sample-1)
-  )
+  (num-window-increases (map edn/read-string (string/split (puzzle-in 1) #"\n"))))
+
+(def sub-commands
+  {"forward"
+   (fn [x]
+     (fn [[horizontal-position depth]]
+       [(+ x horizontal-position) depth]))
+
+   "down"
+   (fn [x]
+     (fn [[horizontal-position depth]]
+       [horizontal-position (+ x depth)]))
+
+   "up"
+   (fn [x]
+     (fn [[horizontal-position depth]]
+       [horizontal-position (- depth x)]))})
+
+(def sample-2
+  "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2")
+
+(defn final-sub-position [commands]
+  (reduce
+   #(%2 %1)
+   [0 0]
+   (map
+    (fn [line]
+      (let [[command x] (string/split line #" ")]
+        ((sub-commands command) (edn/read-string x))))
+    commands)))
+
+(final-sub-position (string/split sample-2 #"\n"))
+
+(apply * (final-sub-position (string/split (puzzle-in 2) #"\n")))
+;; => 2272262
+
+(def aim-sub-commands
+  {"forward"
+   (fn [x]
+     (fn [[horizontal-position depth aim]]
+       [(+ x horizontal-position) (+ depth (* aim x)) aim]))
+
+   "down"
+   (fn [x]
+     (fn [[horizontal-position depth aim]]
+       [horizontal-position depth (+ x aim)]))
+
+   "up"
+   (fn [x]
+     (fn [[horizontal-position depth aim]]
+       [horizontal-position depth (- aim x)]))})
+
+(defn aimed-sub-position [commands]
+  (reduce
+   #(%2 %1)
+   [0 0 0]
+   (map
+    (fn [line]
+      (let [[command x] (string/split line #" ")]
+        ((aim-sub-commands command) (edn/read-string x))))
+    commands)))
+
+(aimed-sub-position (string/split sample-2 #"\n"))
+;; => [15 60 10]
+
+(let [[h d] (aimed-sub-position (string/split (puzzle-in 2) #"\n"))] (* h d))
+;; => 2134882034
+
+
