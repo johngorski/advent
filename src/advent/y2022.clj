89dfadd59c75c055ee9c4e07b3131d6c52db91b2
John Gorski <john.gorski.jr@gmail.com>
2022-12-07 00:25:08 -0800

    2022 day 5

--- src/advent/y2022.clj
+++ src/advent/y2022.clj
@@ -2,7 +2,7 @@
   (:require
    [clojure.edn :as edn]
    [clojure.java.io :as io]
-   [clojure.sets :as sets]
+   [clojure.set :as sets]
    [clojure.string :as string]))
 
 (defn in [day]
@@ -11,6 +11,133 @@
 
 (defn in-lines [day]
   (string/split-lines (slurp (io/resource (str "2022/" day ".txt")))))
+
+;; Day 5
+
+(defn stack [s]
+  (reduce conj () (reverse (map str s))))
+
+(comment
+  {"1" (stack "QGPRLCTF")
+   "2" (stack "JSFRWHQN")
+   "3" (stack "QMPWHBF")
+   "4" (stack "FDTSV")
+   "5" (stack "ZFVWDLQ")
+   "6" (stack "SLCZ")
+   "7" (stack "FDVMBZ")
+   "8" (stack "BJT")
+   "9" (stack "HPSLGBNQ")}
+
+  (first (drop 10 (in-lines 5))))
+;; => "move 1 from 8 to 1"
+
+(defn stack-step [s]
+  (let [[_ rep-str from to]
+        (re-matches #"move (\d+) from (\d+) to (\d+)" s)]
+    (repeat (edn/read-string rep-str) (fn [stacks]
+                                        (let [crate (first (stacks from))]
+                                          (-> stacks
+                                              (update from rest)
+                                              (update to conj crate)))))))
+
+(comment
+  (let [stacks {"1" (stack "NZ")
+                "2" (stack "DCM")
+                "3" (stack "P")}
+        steps ["move 1 from 2 to 1"]]
+    (reduce #(%2 %1) stacks (mapcat stack-step steps)))
+  ;; => {"1" ("D" "N" "Z"), "2" ("C" "M"), "3" ("P")}
+
+  (let [stacks {"1" (stack "NZ")
+                "2" (stack "DCM")
+                "3" (stack "P")}
+        steps ["move 1 from 2 to 1"
+               "move 3 from 1 to 3"]]
+    (reduce #(%2 %1) stacks (mapcat stack-step steps)))
+  ;; => {"1" (), "2" ("C" "M"), "3" ("Z" "N" "D" "P")}
+
+  (let [stacks {"1" (stack "NZ")
+                "2" (stack "DCM")
+                "3" (stack "P")}
+        steps ["move 1 from 2 to 1"
+               "move 3 from 1 to 3"
+               "move 2 from 2 to 1"]]
+    (reduce #(%2 %1) stacks (mapcat stack-step steps)))
+  ;; => {"1" ("M" "C"), "2" (), "3" ("Z" "N" "D" "P")}
+
+  (let [stacks {"1" (stack "NZ")
+                "2" (stack "DCM")
+                "3" (stack "P")}
+        steps ["move 1 from 2 to 1"
+               "move 3 from 1 to 3"
+               "move 2 from 2 to 1"
+               "move 1 from 1 to 2"]]
+    (reduce #(%2 %1) stacks (mapcat stack-step steps))))
+;; => {"1" ("C"), "2" ("M"), "3" ("Z" "N" "D" "P")}
+
+(defn move-crates [stacks procedure]
+  (reduce #(%2 %1) stacks (mapcat stack-step procedure)))
+
+(comment
+  (move-crates
+   {"1" (stack "NZ")
+    "2" (stack "DCM")
+    "3" (stack "P")}
+   ["move 1 from 2 to 1"
+    "move 3 from 1 to 3"
+    "move 2 from 2 to 1"
+    "move 1 from 1 to 2"])
+  ;; => {"1" ("C"), "2" ("M"), "3" ("Z" "N" "D" "P")}
+
+  (let [final-crates
+        (move-crates
+         {"1" (stack "QGPRLCTF")
+          "2" (stack "JSFRWHQN")
+          "3" (stack "QMPWHBF")
+          "4" (stack "FDTSV")
+          "5" (stack "ZFVWDLQ")
+          "6" (stack "SLCZ")
+          "7" (stack "FDVMBZ")
+          "8" (stack "BJT")
+          "9" (stack "HPSLGBNQ")}
+         (drop 10 (in-lines 5)))]
+    (apply str (map (comp first final-crates str inc) (range 9)))))
+;; => "VGBBJCRMN"
+
+(defn stack-9001 [s]
+  (into [] (map str) s))
+
+;; (stack-9001 "abc")
+;; => ["a" "b" "c"]
+
+(defn stack-step-9001 [s]
+  (let [[_ rep-str from to]
+        (re-matches #"move (\d+) from (\d+) to (\d+)" s)
+        crates (edn/read-string rep-str)]
+    (fn [stacks]
+      (let [substack (take crates (stacks from))]
+        (-> stacks
+            (update from #(drop crates %))
+            (update to #(concat substack %)))))))
+
+(defn move-crates-9001 [stacks procedure]
+  (reduce #(%2 %1) stacks (map stack-step-9001 procedure)))
+
+(comment
+  (let [final-crates
+        (move-crates-9001
+         {"1" (stack "QGPRLCTF")
+          "2" (stack "JSFRWHQN")
+          "3" (stack "QMPWHBF")
+          "4" (stack "FDTSV")
+          "5" (stack "ZFVWDLQ")
+          "6" (stack "SLCZ")
+          "7" (stack "FDVMBZ")
+          "8" (stack "BJT")
+          "9" (stack "HPSLGBNQ")}
+         (drop 10 (in-lines 5)))]
+    (apply str (map (comp first final-crates str inc) (range 9)))))
+  ;; => "LBBVJBRMH"
 
 ;; Day 4
 
@@ -94,7 +221,7 @@
   ;; => 27
 
   (reduce + (map (comp item-priority both-compartments-item) (in-lines 3))))
-  ;; => 7793
+;; => 7793
 
 (defn badge-item [rucksacks]
   (first (apply sets/intersection (map set rucksacks))))
@@ -107,7 +234,7 @@
   ;; => \r
 
   (reduce + (map (comp item-priority badge-item) (partition 3 (in-lines 3)))))
-  ;; => 2499
+;; => 2499
 
 ;; Day 2
 
@@ -128,7 +255,7 @@
   (rps-result :Rock :Scissors)
   ;; => :loss
   (rps-result :Scissors :Rock))
-  ;; => :win
+;; => :win
 
 (defn rps-score [them you]
   (let [result (rps-result them you)]
@@ -148,7 +275,7 @@
   (rps-score :Paper :Rock)
   ;; => 1
   (rps-score :Scissors :Scissors))
-  ;; => 6
+;; => 6
 
 (defn rps-decode [key moves]
   (let [[abc xyz] moves]
@@ -193,27 +320,27 @@
   (last (map #(string/split % #"\s+") (in-lines 2)))
   ;; => ["A" "Z"]
   (first (map #(string/split % #"\s+") (in-lines 2))))
-  ;; => ["B" "Y"]
+;; => ["B" "Y"]
 
 (defn rps-2-score [abc xyz]
   (get-in {
-    ;; vs Rock
-    "A" {"X" (+ 0 3)
-         "Y" (+ 3 1)
-         "Z" (+ 6 2)}
-    ;; vs Paper
-    "B" {"X" (+ 0 1)
-         "Y" (+ 3 2)
-         "Z" (+ 6 3)}
-    ;; vs Scissors
-    "C" {"X" (+ 0 2)
-         "Y" (+ 3 3)
-         "Z" (+ 6 1)}
-    } [abc xyz]))
+           ;; vs Rock
+           "A" {"X" (+ 0 3)
+                "Y" (+ 3 1)
+                "Z" (+ 6 2)}
+           ;; vs Paper
+           "B" {"X" (+ 0 1)
+                "Y" (+ 3 2)
+                "Z" (+ 6 3)}
+           ;; vs Scissors
+           "C" {"X" (+ 0 2)
+                "Y" (+ 3 3)
+                "Z" (+ 6 1)}
+           } [abc xyz]))
 
 (comment
   (reduce + (map #(apply rps-2-score %) (map #(string/split % #"\s+") (in-lines 2)))))
-  ;; => 12725
+;; => 12725
 
 ;; Day 1
 (comment
