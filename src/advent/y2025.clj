da06dfd1422e616e5164ebd4c51550b90d8915c4
John Gorski <john.gorski.jr@gmail.com>
2025-12-01 21:32:35 -0800

    Initialize 2025 namespace

---
+++ src/advent/y2025.clj
@@ -0,0 +1,14 @@
+(ns advent.y2025
+  (:require
+   [advent.grid :as grids]
+   [advent.puzzle :as puzzle]
+   [clojure.edn :as edn]
+   [clojure.java.io :as io]
+   [clojure.set :as sets]
+   [clojure.string :as string]
+   [clojure.zip :as zip]
+   [instaparse.core :as insta]
+   ))
+
+
+
